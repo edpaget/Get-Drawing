22c50bb21ebb9b0057b58267c7c2d9fa42a4520d
edpaget <ed.paget@gmail.com>
2010-09-29 18:21:09 -0500

    project started

---
+++ public/getDrawing.js
@@ -0,0 +1,9 @@
+function draw() {
+	var canvas = document.getElementById("drawingArea");
+	var ctx = canvas.getContext("2d");
+
+	ctx.fillStyle = "rgb(200,0,0)";
+	ctx.fillRect (10, 10, 55, 50);
+	
+}
+
