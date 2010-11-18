86e94dae4160add4cd2c77a1cf28d7b632bf4a59
edpaget <ed.paget@gmail.com>
2010-11-17 18:10:03 -0600

    Added database stuff

--- main.rb
+++ main.rb
@@ -2,19 +2,29 @@ require 'rubygems'
 require 'sinatra'
 require 'erb'
 require 'json'
+require'active_record'
 
+ActiveRecord::Base.establish_connection(YAML::load(File.open('database.yml')))
 
 get '/' do
 	@randomCanvas = rand(3) + 1
 	erb :index
 end
 
-get '/:picture' do |title|
+get '/:title' do |title|
 	@picture = "wall" + title
 	erb :wall
 end	
 
-post '/reciever/?' do 
+class Tools < ActiveRecord::Base
+end
+
+post '/:title/reciever/?' do |title|
 	jsonData = params[:data]
 	toolFromJSON = JSON.parse(jsonData)
-end 
+	Tools.create(:tool => toolFromJSON, :picture => title)
+end
+
+get '/sender/:picture' do
+	
+end  
